6c92cac51f9fd91a5960a1f108807c65c2813acd
Aimal Amiri <meaimal@hotmail.com>
2022-05-04 00:03:26 +0430

    Add tests for stringLength function

--- stringLength.js
+++ stringLength.js
@@ -1,5 +1,6 @@
 const stringLength = (string) => {
-  return 1;
+  if (string.length > 0 && string.length < 10) return string.length;
+  throw new Error("String length is less then 1 or greater then 10");
 };
 
 module.exports = stringLength;
--- stringLength.test.js
+++ stringLength.test.js
@@ -3,3 +3,11 @@ const stringLength = require('./stringLength');
 test('Length is one', () => {
   expect(stringLength('a')).toBe(1);
 });
+test('Throw an error if the length is more then 10', () => {
+  expect(() => stringLength('onetwothreefour')).toThrow();
+});
+
+test('Throw an error if the length is less then 1', () => {
+  expect(() => stringLength('')).toThrow();
+});
+
